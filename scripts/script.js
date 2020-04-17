e22adac141bdf69e36f977c44b6725f78f284da3
pafable <pafable@gmail.com>
2020-04-16 22:13:00 -0400

    added apigateway

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
-// alert("Hello world!")
 function SendIt() {
     var NAME = document.getElementById("name-input").value;
     var EMAIL = document.getElementById("email-input").value;
     var data = JSON.stringify({name: NAME, email: EMAIL});
-    // fetch('APIGATEWAY-URL', { 
-    //     method: 'POST', 
-    //     body: data 
-    // });
+    fetch('https://hks34l21be.execute-api.us-east-2.amazonaws.com/woot', { 
+        method: 'POST',
+        'Content-Type': 'application/json',
+        Access-Control-Allow-Origin: *,
+        body: data 
+    });
     console.log(data)
 } 
